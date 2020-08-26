cbd8ed00f27563482d486cdac37346dc3f182a32
zwendelaar <67763799+zwendelaaar@users.noreply.github.com>
2020-08-26 19:27:12 +0200

    Update hackingtool.py

--- hackingtool.py
+++ hackingtool.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 # Version 1.1.0
 import os
+<<<<<<< HEAD
 import sys
 import webbrowser
 import socket
@@ -2999,6 +3000,31 @@ class Main:
 
         if choice == "99":
             self.menu()
+=======
+import webbrowser
+from platform import system
+from time import sleep
+
+from core import HackingToolsCollection
+from tools.anonsurf import AnonSurfTools
+from tools.ddos import DDOSTools
+from tools.exploit_frameworks import ExploitFrameworkTools
+from tools.forensic_tools import ForensicTools
+from tools.information_gathering_tools import InformationGatheringTools
+from tools.other_tools import OtherTools
+from tools.payload_creator import PayloadCreatorTools
+from tools.phising_attack import PhishingAttackTools
+from tools.post_exploitation import PostExploitationTools
+from tools.remote_administration import RemoteAdministrationTools
+from tools.reverse_engineering import ReverseEngineeringTools
+from tools.sql_tools import SqlInjectionTools
+from tools.steganography import SteganographyTools
+from tools.tool_manager import ToolManager
+from tools.webattack import WebAttackTools
+from tools.wireless_attack_tools import WirelessAttackTools
+from tools.wordlist_generator import WordlistGeneratorTools
+from tools.xss_attack import XSSAttackTools
+>>>>>>> parent of 1366a6e... Update hackingtool.py
 
     def uninstall(self):
         self.clear_scr()
